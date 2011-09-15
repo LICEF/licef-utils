f4767b59b75488bf7841452f134142eaae0499cd
Frederic Bergeron <frederic.bergeron@licef.ca>
2011-09-15 16:19:11 +0000

    Added some URIs.
    git-svn-id: svn://hades.licef.ca/licef-commons/licef-utils/trunk@18 bbb71776-42f0-0310-8528-d799e8e14b50

--- src/licef/CommonNamespaceContext.java
+++ src/licef/CommonNamespaceContext.java
@@ -13,6 +13,9 @@ public class CommonNamespaceContext implements NamespaceContext {
     public static final String lomNSURI = "http://ltsc.ieee.org/xsd/LOM";
     public static final String dcNSURI = "http://purl.org/dc/elements/1.1/";
     public static final String oaidcNSURI = "http://www.openarchives.org/OAI/2.0/oai_dc/";
+    public static final String xsdNSURI = "http://www.w3.org/2001/XMLSchema";
+    public static final String xsiNSURI = "http://www.w3.org/2001/XMLSchema-instance";
+    public static final String fedora_managementNSURI = "http://www.fedora.info/definitions/1/0/management/";
 
     public static CommonNamespaceContext getInstance() {
         if( instance == null )
@@ -40,10 +43,16 @@ public class CommonNamespaceContext implements NamespaceContext {
         namespaces.put( "lom", lomNSURI );
         namespaces.put( "dc", dcNSURI );
         namespaces.put( "oaidc", oaidcNSURI );
+        namespaces.put( "xsd", xsdNSURI );
+        namespaces.put( "xsi", xsiNSURI );
+        namespaces.put( "fedora_management", fedora_managementNSURI );
 
         prefixes.put( lomNSURI, "lom" );
         prefixes.put( dcNSURI, "dc" );
         prefixes.put( oaidcNSURI, "oaidc" );
+        prefixes.put( xsdNSURI, "xsd" );
+        prefixes.put( xsiNSURI, "xsi" );
+        prefixes.put( fedora_managementNSURI, "fedora_management" );
     }
 
     private static CommonNamespaceContext instance;
