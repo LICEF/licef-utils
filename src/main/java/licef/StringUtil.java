6481f453f1a4c26f04e3ed44eebbd83a9145c248
Alexis Miara <alexis.miara@licef.ca>
2012-05-29 14:12:19 +0000

    unquote string method
    git-svn-id: svn://hades.licef.ca/licef-commons/licef-utils/trunk@56 bbb71776-42f0-0310-8528-d799e8e14b50

--- src/main/java/licef/StringUtil.java
+++ src/main/java/licef/StringUtil.java
@@ -233,4 +233,22 @@ public class StringUtil {
         }
         return result;
     }
+
+    /**
+     * Unquote a string
+     * @param str
+     * @return unquoted string
+     */
+    public static String unquote(String str) {
+        int length = str == null ? -1 : str.length();
+        if( str == null || length == 0 )
+            return str;
+
+        if( length > 1 && str.charAt( 0 ) == '\"' && str.charAt( length - 1 ) == '\"' ) {
+            str = str.substring( 1, length - 1 );
+        }
+
+        return str;
+    }
+    
 }
