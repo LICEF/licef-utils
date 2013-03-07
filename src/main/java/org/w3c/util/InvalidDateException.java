59af35eb224c785cf831dbe77ec728af7e25429d
Frederic Bergeron <frederic.bergeron@licef.ca>
2013-03-07 13:33:13 +0000

    Added DateParser class.
    git-svn-id: svn://hades.licef.ca/licef-commons/licef-utils/trunk@69 bbb71776-42f0-0310-8528-d799e8e14b50

---
+++ src/main/java/org/w3c/util/InvalidDateException.java
@@ -0,0 +1,15 @@
+// InvalidDateException.java
+// (c) COPYRIGHT MIT, INRIA and Keio, 2000.
+// Please first read the full copyright statement in file LICENSE.w3c.html
+package org.w3c.util;
+
+/**
+ * @author  Benoit Mahe (bmahe@w3.org)
+ */
+public class InvalidDateException extends Exception {
+
+  public InvalidDateException(String msg) {
+    super(msg);
+  }
+
+}
