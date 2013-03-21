e4a224f9e5fb0bb8013bc2a85e12f343f709cc42
Frederic Bergeron <frederic.bergeron@licef.ca>
2013-03-21 08:50:48 +0000

    Improved functions in LangUtil.
    git-svn-id: svn://hades.licef.ca/licef-commons/licef-utils/trunk@72 bbb71776-42f0-0310-8528-d799e8e14b50

--- src/main/java/licef/LangUtil.java
+++ src/main/java/licef/LangUtil.java
@@ -6,17 +6,57 @@ import java.util.Map;
 
 public class LangUtil {
 
-    public static String getISO2Language( String langCode, boolean returnNullIfNotFound ) {
+    /**
+     * Obtains a ISO-639-2 compliant equivalent language code. 
+     * @param langCode A 2 or 3 characters long language code like fr or fre.
+     * @return A corresponding language code from the ISO-639-2 table or a lowercased value of langCode if langCode is unknown to the system.
+     */
+    public static String getISO2Language( String langCode ) {
+        return( getISO2Language( langCode, false ) );
+    }
+
+    /**
+     * Obtains a ISO-639-2 compliant equivalent language code. 
+     * @param langCode A 2 or 3 characters long language code like fr or fre.
+     * @param returnsNullIfNotFound <code>null</code> if the language code is not known by the system or lowercased langCode otherwise.
+     * @return A corresponding language code from the ISO-639-2 table.
+     */
+    public static String getISO2Language( String langCode, boolean returnsNullIfNotFound ) {
+        if( langCode == null )
+            return( null );
+
         langCode = langCode.toLowerCase();
         String lang = langISO2.get( langCode );
-        if( returnNullIfNotFound )
+        if( returnsNullIfNotFound )
             return( lang == null ? null : lang );
         else
             return( lang == null ? langCode : lang );
     }
 
-    public static String getISO2Language( String langCode ) {
-        return( getISO2Language( langCode, false ) );
+    /**
+     * Converts a language code like fr_CA to fr-CA or fre-FR to fr-FR.
+     * @param langCode A language code that may contain either a 2 or 3 characters long language and an optional 2-characters long country code.
+     * @return The same language code except that its language part will be converted to ISO-639-2 if possible.
+     */
+    public static String convertLangToISO2( String langCode ) {
+        return( convertLangToISO2( langCode, "-" ) );
+    }
+
+    /**
+     * Converts a language code like fr_CA to fr-CA or fre-FR to fr-FR.
+     * @param langCode A language code that may contain either a 2 or 3 characters long language and an optional 2-characters long country code.
+     * @param delimiter Character used to separate the language and country parts.
+     * @return The same language code except that its language part will be converted to ISO-639-2 if possible.
+     */
+    public static String convertLangToISO2( String langCode, String delimiter ) {
+        if( langCode == null )
+            return( null );
+
+        String[] parts = langCode.split( "[-_]" );
+        String lang = parts[ 0 ];
+        String country = ( parts.length > 1 ? parts[ 1 ] : null );
+        String convertedLang = LangUtil.getISO2Language( lang ) + ( country == null ? "" : delimiter + country );
+        return( convertedLang );
     }
 
     private static Map<String, String> langISO2; // Contains entries like fre -> fr, spa -> es, etc.
